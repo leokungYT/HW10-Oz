c43df28de08a976f09aa2ebd8ed59f727cbef64c
กูลีโอ จุฟๆ <111227729+leokungYT@users.noreply.github.com>
2023-12-25 20:29:42 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+const root = ReactDOM.createRoot(document.getElementById("root"));
+let isAdmin = true;
+
+
+root.render(
+  <>
+    <h1 class="show-sum">Sum = 0</h1>
+    <div class="ozxx">
+    <button class="btn-add">Add Counter</button>
+    </div>
+    <div class="ozx">
+      <div class="oz">
+        <button class="btn1">+</button>
+        <h3 class="number">0</h3>
+        <button class="btn2">-</button>
+        <button class="btn3">C</button>
+        <button class="btn3">X</button>
+      </div>
+    </div>
+  </>
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//jirasak primkumlai
